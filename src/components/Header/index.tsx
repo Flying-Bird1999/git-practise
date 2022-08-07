93dd330ba54f61bcd42616dec8a77044dc5c0397
zengxiaocan <zengxiaocan@joyy.com>
2022-08-07 11:10:45 +0800

    fix: hoxfix分支上线

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 function Header() {
   useEffect(() => {
     console.log('临时修复线上bug');
+    console.log('hoxfix分支开发！！！');
   }, []);
 
   return <span>Header</span>;
--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,6 @@ import './index.css';
 
 console.log('开发main dev分支');
 
-console.log('二次开发main');
-
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
