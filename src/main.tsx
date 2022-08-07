acd0657e52cb9fc36a810f8eb9c5accc113e83bf
zengxiaocan <zengxiaocan@joyy.com>
2022-08-07 11:03:59 +0800

    fix: 二次开发...

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,8 @@ import './index.css';
 
 console.log('开发main dev分支');
 
+console.log('二次开发main');
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
