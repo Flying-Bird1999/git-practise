82d79e1f7567bd5313e6537f450df2b31780eb89
zengxiaocan <zengxiaocan@joyy.com>
2022-08-07 11:33:43 +0800

    fix: hoxfix分支开发

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,8 +1,10 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 function Header() {
+  const [count, setCount] = useState(1);
   useEffect(() => {
     console.log('main分支');
-  }, []);
+    console.log('hoxfix分支');
+  }, [count]);
 
   return <span>Header</span>;
 }
