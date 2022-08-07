2034d56239c68f9e27a34c68df9fe54e4cc165ba
zengxiaocan <zengxiaocan@joyy.com>
2022-08-07 11:00:18 +0800

    fix: 修改bug

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <Header></Header>
-      <span>改bug。。。</span>
+      <span>修改App.tsx bug--- dev分支</span>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src="/vite.svg" className="logo" alt="Vite logo" />
